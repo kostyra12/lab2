32e2ea1267c5b73bf2b18fe62005a2300c01205a
KostyaGridnev <kostya-gridnev@mail.ru>
2021-04-15 18:45:08 +0300

    пятый

--- lab5/components/quotes.php
+++ lab5/components/quotes.php
@@ -1,11 +1,7 @@
 <section class="quotes container">
-  <h2 class="section-title">Любимые цитаты</h2>
+  <h2 class="section-title">Любимая цитата</h2>
   <blockquote>
     <p>Плохие программисты думают о коде. Хорошие программисты думают о структурах данных и их взаимосвязях.</p>
     <cite>Линус Торвальдс</cite>
-  </blockquote>
-  <blockquote>
-    <p>Хорошему коту и в феврале март.</p>
-    <cite>Кекс</cite>
   </blockquote>
 </section>
--- lab5/index.php
+++ lab5/index.php
@@ -1,13 +1,13 @@
 <?php
-$title = 'Мой сайт';
-$name = 'Неопознанный енот';
-$image = 'img/unknown-raccoon.svg';
-$email = 'enot_neopoznanniy@gmail.com';
-$phone = '+79876543210';
+$title = 'Сайт Кости';
+$name = 'Опознанный енот';
+$image = 'img/orbnn0rf_88.jpg';
+$email = 'kostya-gridnev@mail.ru';
+$phone = '+79855553535';
 
 require('components/header.php');
-require('components/about.php');
 require('components/status.php');
+require('components/about.php');
 require('components/photos.php');
 require('components/news.php');
 require('components/quotes.php');
